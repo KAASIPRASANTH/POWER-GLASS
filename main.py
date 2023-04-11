dd1f27af90c30138dd00a9e0c377b52f5acc6f88
KAASIPRASANTH <kaasiprasanth.a2020cse@sece.ac.in>
2023-04-11 15:34:04 +0530

    commit

--- main.py
+++ main.py
@@ -3,18 +3,25 @@ tess.pytesseract.tesseract_cmd = r'C:\Users\ADMIN\AppData\Local\Tesseract-OCR\te
 from PIL import Image
 import pyttsx3
 import cv2
+import gtts as gt
+import os
+
+
 #project
 def speech(text):
     engine = pyttsx3.init()
+
     engine.setProperty("rate",125)
     voices = engine.getProperty('voices')
-    engine.setProperty("voice",voices[0].id)
+    engine.setProperty("voice",voices[1].id)
     engine.say(text)
     engine.runAndWait()
 
 
 cam = cv2.VideoCapture(0)
-img_counter = 2;
+img_counter = 1;
+
+'''
 while True:
     ret,frame = cam.read()
 
@@ -30,12 +37,38 @@ while True:
         break
     #space key -> To take the photo click on to the space button
     elif k%256 == 32:
-        img_name = "sample_{}.png".format(img_counter)
+        img_name = "sample{img_counter}.png".format(img_counter)
         cv2.imwrite(img_name,frame)
+        
         print("screenshot taken")
+'''
 
 
-img = Image.open('sample.png')
-text = tess.image_to_string(img)
-print(text)   
-speech(text)
+
+
+
+imageUrl = 'sample'+(str)(img_counter)+'.png'
+img = Image.open('japan.png')
+text1 = tess.image_to_string(img,lang='jpn')
+print(text1)   
+speech(text1)
+
+
+
+'''
+To save audio file 
+
+tts = gt.gTTS(text=text1,lang="ta")
+tts.save("TamilAud.mp3")
+os.system("TamilAud.mp3")
+'''
+
+
+
+
+'''
+eng = 0
+japan = 1
+tamil = 2
+hindi = 3
+'''
